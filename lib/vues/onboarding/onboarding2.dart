f95e5c642bb23bc71255e842b15a5b78d538b3e6
Le Pro <ngouoghejunior@gmail.com>
2022-11-01 09:57:29 +0100

    Implemented splash screen

---
+++ lib/vues/onboarding/onboarding2.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:landflight/vues/onboarding/onboarding3.dart';
+import 'package:landflight/vues/onboarding/onboarding_base.dart';
+
+class Onboarding2 extends StatelessWidget {
+  const Onboarding2({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return OnboardingBase(
+        imagePath: "lib/assets/images/onboarding2.png",
+        title: "Choisissez votre destination",
+        description:
+            "Parmi nos nombreuses agences partenaires, choisissez l’agence qui vous inspire et arrivez en toute sécurité",
+        index: 1,
+        back: () {
+          retour(context);
+        },
+        next: () {
+          suivant(context);
+        });
+  }
+
+  retour(BuildContext context) {
+    Navigator.pop(context);
+  }
+
+  suivant(BuildContext context) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => Onboarding3()),
+    );
+  }
+}
---
+++ lib/vues/onboarding/onboarding3.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:landflight/vues/onboarding/onboarding_base.dart';
+
+class Onboarding3 extends StatelessWidget {
+  const Onboarding3({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return OnboardingBase(
+        imagePath: "lib/assets/images/onboarding3.png",
+        title: "Emmenez votre famille",
+        description:
+            "Commandez autant de billet que possible, nos offres et cadeaux serons à votre merci.",
+        index: 2,
+        back: () {
+          retour(context);
+        },
+        next: () {
+          suivant(context);
+        });
+  }
+
+  retour(BuildContext context) {
+    Navigator.pop(context);
+  }
+
+  suivant(BuildContext context) {
+    // Navigator.push(
+    //   context,
+    //   MaterialPageRoute(
+    //       builder: (context) => Home()),
+    // );
+  }
+}
